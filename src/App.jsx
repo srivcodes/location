7769dac91b35f074f1ff178314a7faa04c15b21e
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-09-22 19:12:09 +0200

    feature: made the basic UI structure of the web-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,61 @@
-import { useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { GeoJSON, MapContainer, TileLayer } from 'react-leaflet';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
+
 import { fetchData } from './service';
 
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(typeof position.coords.latitude);
-      // fetchData(34.96882, 33.66956, 35.21882, 33.91956);
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-      fetchData(latitude, longitude, latitude + 0.25, longitude + 0.25);
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+
+      const fetchedData = await fetchData(
+        latitude,
+        longitude,
+        latitude + 0.25,
+        longitude + 0.25
+      );
+      setData(fetchedData);
     });
-  }, []);
+  }, [latitude, longitude]);
 
   return (
     <div className="App">
+      <div className='Content'>
       <h1>Location</h1>
-      <input className="input-field" placeholder='Please Enter the Coordinates...'></input>
-      <div className="card">
-        {/* <button>count</button> */}
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <input
+        className="input-field"
+        placeholder="Please Enter the Coordinates..."
+      ></input>
+      <div>
+       <h3>Dataset</h3>
+          <p>Latitude:</p>
+          <p>Longitude:</p>
+          <p>Timestamp:</p>
       </div>
-      <p className="read-the-docs">Say what????</p>
+      </div>
+      {latitude > 0 && longitude > 0 && (
+        <div className="card">
+          <MapContainer
+            zoom={20}
+            scrollWheelZoom={false}
+            center={{ lat: latitude, lng: longitude }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <GeoJSON key={JSON.stringify(data)} data={data} />
+          </MapContainer>
+        </div>
+      )}
     </div>
   );
 }
--- src/service.js
+++ src/service.js
@@ -2,7 +2,7 @@ import osmtogeojson from 'osmtogeojson';
 import axios from 'axios';
 
 export async function fetchData(a, b, c, d) {
-  console.log(a, b, c, d);
+  // console.log(a, b, c, d);
   let response = await axios.get(
     'https://www.openstreetmap.org/api/0.6/map?bbox=' +
       encodeURIComponent(`${a},${b},${c},${d}`)
